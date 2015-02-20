0c981cfb8b7a773dc856e5af252d47b94b860874
Nicolas Trangez <ikke@nicolast.be>
2015-02-20 19:55:46 +0100

    Implement HTTP calls using Wreq

--- src/Network/OpenStack/Client/Wreq.hs
+++ src/Network/OpenStack/Client/Wreq.hs
@@ -8,7 +8,9 @@ module Network.OpenStack.Client.Wreq (
       runOperation
     ) where
 
+import Control.Lens hiding (op)
 import Data.Aeson
+import qualified Network.Wreq as W
 
 import Network.OpenStack.Client
 
@@ -19,24 +21,26 @@ runOperation op = case op of
     OPUT -> runPUT op
     ODELETE -> runDELETE op
 
-runGET :: Handler 'GET () IO b
+runGET :: FromJSON b => Handler 'GET () IO b
 runGET op url = do
     let full = url ++ operationSuffix op
-    putStrLn full
-    return undefined
+    resp <- W.asJSON =<< W.get full
+    return $ resp ^. W.responseBody
 
 runPOST :: (ToJSON a, FromJSON b) => Handler 'POST a IO b
-runPOST op url value = do
+runPOST op url a = do
     let full = url ++ operationSuffix op
-    putStrLn $ "POST " ++ full
-    print $ encode value
-    putStrLn ""
-    case eitherDecode "[1, 2, 3]" of
-        Left e -> error e
-        Right (v :: b) -> return v
+    resp <- W.asJSON =<< W.post full (toJSON a)
+    return $ resp ^. W.responseBody
 
-runPUT :: Handler 'PUT a IO b
-runPUT = undefined
+runPUT :: (ToJSON a, FromJSON b) => Handler 'PUT a IO b
+runPUT op url a = do
+    let full = url ++ operationSuffix op
+    resp <- W.asJSON =<< W.put full (toJSON a)
+    return $ resp ^. W.responseBody
 
 runDELETE :: Handler 'DELETE () IO ()
-runDELETE = undefined
+runDELETE op url = do
+    let full = url ++ operationSuffix op
+    _ <- W.delete full
+    return ()
